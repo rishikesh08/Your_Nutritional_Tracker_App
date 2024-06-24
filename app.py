2c056cec5e0a3853ab42763940cb65fbee5c1fb9
Rishikesh Kumar <marketsingh16@gmail.com>
2024-06-24 23:21:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ if uploaded_file is not None:
 submit=st.button("Tell me about the Nutritional content!!")
 
 input_prompt="""
+If the image uploaded is not of any food item respond as 'Please upload a valid image'.
 You are an expert in nutritionist where you need to see the food items of the image and calculate
 the total calories, also provide the details of every food items with the calorie intake 
 in the below format:
